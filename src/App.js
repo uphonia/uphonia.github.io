06950e5b2d09af9ad8c7b61e4b0ab8f95f154625
uphonia <wu.angela0@gmail.com>
2021-09-03 21:47:43 -0400

    Update summary. Add 'top of page' button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import './index.css';
-import Home from './Home'
+import Summary from './Summary'
 import Works from './Works'
 import Navbar from './Navbar'
 import Contact from './Contact'
 import {HashRouter} from 'react-router-dom'
 
 function App() {
+	function goToTop() {
+		document.body.scrollTop = 0;
+		document.documentElement.scrollTop = 0;
+	}
+
 	return (
 		<HashRouter>
 			<div className="body-container">
 				<Navbar />
 				<hr className="solid"/>
 				<div className="inner-container home-container">
-					<Home />
+					<Summary />
 				</div>
 				<div className="inner-container works-container">
 					<Works />
@@ -22,6 +27,7 @@ function App() {
 					<Contact />
 				</div>
 			</div>
+			<button onClick={goToTop} className="return-btn"></button>
 		</HashRouter>
 	);
 }
--- src/Summary.js
+++ src/Summary.js
@@ -4,45 +4,47 @@ import {SiNuxtDotJs} from 'react-icons/si'
 
 function Summary() {
 	return (
-		<div className="info-container">
-			<h1 className="main-header">
-				Hi, I'm Angela Wu
-			</h1>
-			<ul>
-				<li style={{flexDirection:"row"}}>
-					<IoLocationOutline className="info-icon"/>
-					New York City
-				</li>
-				<li>
-					<div className="info-header">
-						<SiNuxtDotJs className="info-icon"/>
-						<span>Education:</span>
-					</div><br/>
-					<div className="info-text">
-						New York University <br/>
-						Bachelor's in Computer Science <br/>
-						Minor in Game Design
-					</div>
-				</li>
-				<li>
-					<div className="info-header">
-						<SiNuxtDotJs className="info-icon"/>
-						<span>Current role:</span>
-					</div><br/>
-					<div className="info-text">
-						Software Engineer @ Modrokkr
-					</div>
-				</li>
-				<li>
-					<div className="info-header">
-						<SiNuxtDotJs className="info-icon"/>
-						<span>Status:</span>
-					</div><br/>
-					<div className="info-text">
-						I am open to new opportunities that are permanently remote from anywhere (or just North America). I am open to front-end or full-stack roles. Please reach out if you would like to learn more!
-					</div>
-				</li>
-			</ul>
+		<div className="sub-container">
+			<div className="info-container">
+				<h1 className="main-header">
+					Hi, I'm Angela Wu
+				</h1>
+				<ul>
+					<li style={{flexDirection:"row"}}>
+						<IoLocationOutline className="info-icon"/>
+						New York City
+					</li>
+					<li>
+						<div className="info-header">
+							<SiNuxtDotJs className="info-icon"/>
+							<span>Education:</span>
+						</div><br/>
+						<div className="info-text">
+							New York University <br/>
+							Bachelor's in Computer Science <br/>
+							Minor in Game Design
+						</div>
+					</li>
+					<li>
+						<div className="info-header">
+							<SiNuxtDotJs className="info-icon"/>
+							<span>Current role:</span>
+						</div><br/>
+						<div className="info-text">
+							Software Engineer @ Modrokkr
+						</div>
+					</li>
+					<li>
+						<div className="info-header">
+							<SiNuxtDotJs className="info-icon"/>
+							<span>Status:</span>
+						</div><br/>
+						<div className="info-text">
+							I am open to new opportunities that are permanently remote from anywhere (or just North America). I want to work more with React and am open to front-end or full-stack roles. Please reach out if you would like to learn more!
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	)
 }
